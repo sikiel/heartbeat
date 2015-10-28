8283378676e557492c54b3460eb6e85ed2e7b3f4
sikiel <agata.chochowska@gmail.com>
2015-10-28 11:19:11 +0100

    maven project created

---
+++ heartbeat/src/main/java/com/capgemini/heartbeat/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.heartbeat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
