2628c12b48bbf7fd35d695c8062fee991ead28c1
advena <m.gepert@gmail.com>
2015-10-29 10:37:44 +0100

    add getters for fields in TaskResult

--- heartbeat/src/main/java/com/capgemini/heartbeat/TaskResult.java
+++ heartbeat/src/main/java/com/capgemini/heartbeat/TaskResult.java
@@ -11,5 +11,17 @@ public class TaskResult {
 		this.name = name;
 		this.status = status;
 	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public String getStatus() {
+		return status;
+	}
 
 }
