3412c895dc9de67adb625755dac1f778c55bf9d0
sikiel <agata.chochowska@gmail.com>
2015-10-30 15:54:52 +0100

    reverted change in csv file creator

--- heartbeat/src/main/java/com/capgemini/heartbeat/CSVReportCreator.java
+++ heartbeat/src/main/java/com/capgemini/heartbeat/CSVReportCreator.java
@@ -1,11 +1,9 @@
 package com.capgemini.heartbeat;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class CSVReportCreator {
 
@@ -13,7 +11,8 @@ public class CSVReportCreator {
 	private static final String endOfLine = "\n";
 	private static final String fileHeader = "timestamp; Jenkins|Grid; Status ";
 	public String pathWithCsvFile = "C:\\heartbeatReport\\heartbeatReport.csv";
-	private boolean needsHeader = false;
+	File file = null;
+	FileWriter writeToCsv = null;
 
 	public CSVReportCreator() {
 
@@ -23,46 +22,45 @@ public class CSVReportCreator {
 		this.pathWithCsvFile = path;
 	};
 
-	private void fillTheCsvFile(List<TaskResult> list, FileWriter writeToCsv) throws IOException {
-		if (needsHeader) {
-			writeToCsv.append(fileHeader);
-			writeToCsv.append(endOfLine);
-		}
-		for (int i = 0; i < list.size(); i++) {
-			writeToCsv.append(list.get(i).getTimestamp().toString());
-			writeToCsv.append(separator);
-			writeToCsv.append(list.get(i).getName());
-			writeToCsv.append(separator);
-			writeToCsv.append(list.get(i).getStatus());
-			writeToCsv.append(endOfLine);
-		}
-	}
+	public void createReport(Object results) {
+		@SuppressWarnings("unchecked")
+		ArrayList<TaskResult> listResults = (ArrayList<TaskResult>) results;
 
-	private FileWriter createCsvFileWriter() throws IOException {
-		File file = new File(this.pathWithCsvFile);
-		if (!file.exists()) {
-			file.getParentFile().mkdirs();
-			file.createNewFile();
-			needsHeader = true;
-		}
-		return new FileWriter(file.getAbsolutePath(), true);
-	}
-
-	private void closeCsvFile(FileWriter writeToCsv) throws IOException{
-		if (writeToCsv != null) {
-			writeToCsv.flush();
-			writeToCsv.close();
-			System.out.println("CSV file updated!");
-		}
-	}
-
-	public void createReport(List<TaskResult> list) {
 		try {
-			FileWriter csvWriter = createCsvFileWriter();
-			fillTheCsvFile(list, csvWriter);
-			closeCsvFile(csvWriter);
+			file = new File(pathWithCsvFile);
+			if (!file.exists()) {
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+				writeToCsv = new FileWriter(file.getAbsolutePath(), true);
+				writeToCsv.append(fileHeader);
+				writeToCsv.append(endOfLine);
+			}else{
+				writeToCsv = new FileWriter(file.getAbsolutePath(), true);
+			}
+
+			for (int i = 0; i < listResults.size(); i++) {
+				writeToCsv.append(listResults.get(i).getTimestamp().toString());
+				writeToCsv.append(separator);
+				writeToCsv.append(listResults.get(i).getName());
+				writeToCsv.append(separator);
+				writeToCsv.append(listResults.get(i).getStatus());
+				writeToCsv.append(endOfLine);
+			}
 		} catch (IOException e) {
+			System.out.println("File open error! ");
 			e.printStackTrace();
-		} 
+		} finally {
+			try {
+				
+				if (writeToCsv != null) {
+					writeToCsv.flush();
+					writeToCsv.close();
+					System.out.println("CSV file updated!");
+				}
+			} catch (IOException e) {
+				System.out.println("File flush or close error! ");
+				e.printStackTrace();
+			}
+		}
 	}
 }
