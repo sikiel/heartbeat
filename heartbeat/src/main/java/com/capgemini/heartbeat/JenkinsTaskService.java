95a20d378336efe2e0b4602a22266256a8ad456b
Bartlomiej Wojcik <BWOJCIK@CE12385.corp.capgemini.com>
2015-11-04 11:51:21 +0100

    checkBuilds - working version 1.0

--- heartbeat/src/main/java/com/capgemini/heartbeat/JenkinsTaskService.java
+++ heartbeat/src/main/java/com/capgemini/heartbeat/JenkinsTaskService.java
@@ -1,7 +1,9 @@
 package com.capgemini.heartbeat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
@@ -10,7 +12,16 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.w3c.dom.CharacterData;
 
 public class JenkinsTaskService implements TaskService {
 	private static final Logger LOG = Logger.getLogger(JenkinsTaskService.class.getName());
@@ -25,9 +36,65 @@ public class JenkinsTaskService implements TaskService {
 	}
 
 	public ResultCollector getTasksResult() {
+		try {
+			checkBuilds();
+		} catch (IOException | SAXException | ParserConfigurationException e) {
+			LOG.severe("Build check failed");
+		}
 		prepareTasks();
 		return resultCollector;
 	}
+
+	private void checkBuilds()throws IOException, SAXException, ParserConfigurationException {
+		Iterator<JenkinsBuild> it = JenkinsBuildList.getBuildList().iterator();
+		while (it.hasNext()) {
+
+			JenkinsBuild jb = (JenkinsBuild) it.next();
+			URL url = new URL(jb.getJenkinsProperty().getUrl() + "/job/" + jb.getJenkinsProperty().getJobName()
+					+ "/lastBuild/api/xml?tree=result,building");
+			String userpass = jb.getJenkinsProperty().getUsername() + ":" + jb.getJenkinsProperty().getPassword();
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
+			conn.setRequestProperty("Authorization", basicAuth);
+			conn.setRequestMethod("GET");
+			LOG.info(conn.getResponseMessage());
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String inputLine;
+			String xml = "";
+			while ((inputLine = in.readLine()) != null) {
+				xml += inputLine;
+			}
+
+			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			InputSource is = new InputSource();
+			is.setCharacterStream(new StringReader(xml));
+			org.w3c.dom.Document doc = db.parse(is);
+
+			NodeList result = doc.getElementsByTagName("result");
+			Element elementResult = (Element) result.item(0);
+			NodeList building = doc.getElementsByTagName("building");
+			Element elementBuilding = (Element) building.item(0);
+
+			if (getCharsFromElement(elementBuilding) == "true") {
+				String name = "JENKINS - " + jb.getJenkinsProperty().getUrl();
+				Long timestamp = new Date().getTime();
+				resultCollector.addResult(new TaskResult(timestamp, name, "IN PROGRESS"));
+				Long timeout = timestamp - jb.getTimestamp();
+				if (timeout > 900000) {
+					buildList.delete(jb);
+				}
+			} else {
+				String name = "JENKINS - " + jb.getJenkinsProperty().getUrl();
+				Long timestamp = new Date().getTime();
+				resultCollector.addResult(new TaskResult(timestamp, name, getCharsFromElement(elementResult)));
+				buildList.delete(jb);
+			}
+
+		}
+	}
+		
+	
 
 	private void prepareTasks() {
 		Iterator<Property> iter = properties.iterator();
@@ -63,6 +130,17 @@ public class JenkinsTaskService implements TaskService {
 		} else {
 			return false;
 		}
+	}
+	
+	private static String getCharsFromElement(Element e) {
+
+		Node child = e.getFirstChild();
+		if (child instanceof CharacterData) {
+			CharacterData cd = (CharacterData) child;
+			return cd.getData();
+		}
+		return "";
+
 	}
 
 	/*
